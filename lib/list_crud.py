34efc0d8f162ed17a5cd4f3a595da2335342b69b
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-03 15:04:23 +0100

    all passed

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,52 @@
 def create_an_empty_list():
-    return None
+    
+    return []
+print(create_an_empty_list())
 
 def create_a_list():
-    return None
+    num = ['money', 'wealth', 'power', 'fame']
+    return num
+print(create_a_list())
+
+
+list = [1,2,3,4]
+fig = 5
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    # print(l)
+    
+    return l
+print(add_element_to_end_of_list(list, fig))
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
+
+print(add_element_to_start_of_list(list, fig))
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
+print(remove_element_from_end_of_list(list))
 
 def remove_element_from_start_of_list(l):
-    return None
+    del(l[0])
+    return l
+print(remove_element_from_start_of_list(list))
+
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
+
+print(retrieve_first_element_from_list(list))
 
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
+print(retrieve_element_from_index(list, 2))
 
 def retrieve_last_element_from_list(l):
-    return None
+    
+    return l[-1]
+
+print(retrieve_last_element_from_list(list))
